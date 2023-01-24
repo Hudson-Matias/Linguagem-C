2fede5ac5b6b5ef0cd984141d4ee07afbe53d09a
Hudson Matias <116506548+Hudson-Matias@users.noreply.github.com>
2023-01-24 15:54:33 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,prod;
+
+    printf("Deseja a tabuada para qual valor? ");
+    scanf("%d", &n);
+
+    for(i=1;i<=10;i++){
+        prod=n*i;
+        printf("%d X %d = %d\n",n,i,prod);
+    }
+
+    return 0;
+}
